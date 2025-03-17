b077867138df50857ed0287a5c3ff7df1fff94f4
juan312 <juanalbani48@gmail.com>
2025-03-17 11:38:40 -0300

    Added test entities to start with the implementarion of the Conceptual Modelling screens

--- backend/src/data/index.ts
+++ backend/src/data/index.ts
@@ -1,2 +1,8 @@
+export * from './mongo/models/deletion-request.model'
+export * from './mongo/models/project.model'
+export * from './mongo/models/revision.model'
 export * from './mongo/models/user.model'
+export * from './mongo/models/verifier-request.model'
+export * from './mongo/models/version.model'
+
 export * from './mongo/mongo-database';
--- backend/src/presentation/server.ts
+++ backend/src/presentation/server.ts
@@ -1,49 +1,83 @@
 import express, { Router } from "express";
+import { ProjectModel, UserModel, VersionModel } from "../data";
 
 interface Options {
-  port?: number;
-  routes: Router;
+	port?: number;
+	routes: Router;
 }
 export class Server {
-  private app = express();
-  private port: number;
-  private routes: Router;
-  private serverListener?: any;
+	private app = express();
+	private port: number;
+	private routes: Router;
+	private serverListener?: any;
 
+	constructor(options: Options) {
+		const { port, routes } = options;
+		this.port = port ?? 3000;
+		this.routes = routes;
+	}
 
-  constructor(options: Options) {
-    const { port, routes } = options;
-    this.port = port ?? 3000;
-    this.routes = routes;
-  }
+	//punto de inicio de la aplicacion
+	async start() {
+		//* para servir todo lo que tengo en mi carpeta publica uso un middleware
+		//middleware - funciones que se ejecutan cada vez que el codigo pase por x ruta y antes del controller
 
-  //punto de inicio de la aplicacion
-  async start() {
-    //* para servir todo lo que tengo en mi carpeta publica uso un middleware
-    //middleware - funciones que se ejecutan cada vez que el codigo pase por x ruta y antes del controller
+		//* Middlewares
+		this.app.use(express.json()); // raw - extrae el body en un jsin
+		this.app.use(express.urlencoded({ extended: true })); // x-www-form-urlencoded - extrae el form en json
 
-    //* Middlewares
-    this.app.use( express.json() ); // raw - extrae el body en un jsin
-    this.app.use( express.urlencoded({ extended: true }) ); // x-www-form-urlencoded - extrae el form en json
-    
+		//* Public folder
+		this.app.use(express.static("/public"));
 
-    //* Public folder
-    this.app.use(express.static("/public"));
+		//* Routes
+		this.app.use(this.routes);
 
-    //* Routes
-    this.app.use(this.routes);
+		console.log("server started");
 
-    console.log("server started");
+		//pongo a la app a escuchar peticiones
+		this.serverListener = this.app.listen(this.port, () => {
+			console.log(`Server running on port ${this.port}`);
+		});
 
-    //pongo a la app a escuchar peticiones
-    this.serverListener = this.app.listen(this.port, () => {
-      console.log(`Server running on port ${ this.port }`);
+    //Uncomment when needed
+		//this.createTestEntities();
+	}
+
+	private createTestEntities() {
+    //Warning! - Drops all collections to allow for a fresh start 
+    UserModel.collection.drop();
+    ProjectModel.collection.drop();
+    VersionModel.collection.drop();
+
+		const user = new UserModel({
+			name: "Juan",
+			lastName: "Albani",
+			email: "juanalbani48@gmail.com",
+      password: "hola123",
+      emailValidated: true,
+      roles: ["MODELADOR"]
+		});
+
+		user.save();
+
+    const version = new VersionModel({
+      title: "Version 1"
     });
 
-  }
+    version.save();
 
-  public close() {
-    this.serverListener?.close();
-  }
-  
+    const project = new ProjectModel({
+      name: "P1",
+      description: "Whatever",
+      owner: user._id,
+    });
+
+    project.versions.push(version._id)
+
+    project.save();
+	}
+
+	public close() {
+		this.serverListener?.close();
+	}
 }
