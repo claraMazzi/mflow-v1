e767c7f983698cb9289bc736cd055f6f4c136fd5
juan312 <juanalbani48@gmail.com>
2025-10-23 18:58:35 -0300

    Corrected the Create and Update Project endpoints

--- backend/src/domain/dtos/project/update-project.dto.ts
+++ backend/src/domain/dtos/project/update-project.dto.ts
@@ -3,17 +3,19 @@
 export class UpdateProjectDto {
   constructor(
     public readonly id: string,
+    public readonly owner: string,
     public readonly title?: string,
     public readonly description?: string,
   ) {}
 
   static create(object: { [key: string]: any }): [string?, UpdateProjectDto?] {
-    const { title, description, id } = object;
+    const { title, description, id, owner } = object;
     if (!id) return ["Missing id"];
+    if (!owner) return ["Missing owner"];
   
     return [
       undefined,
-      new UpdateProjectDto(id, title, description),
+      new UpdateProjectDto(id, owner, title, description),
     ];
   }
 }
