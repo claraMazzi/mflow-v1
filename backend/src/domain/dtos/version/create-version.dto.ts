f4a668b9314de94c997daa9ff3a5f897fbc5b89d
juan312 <juanalbani48@gmail.com>
2025-11-01 18:16:08 -0300

    Fixed a misspelling error

--- backend/src/domain/dtos/version/create-version.dto.ts
+++ backend/src/domain/dtos/version/create-version.dto.ts
@@ -11,7 +11,7 @@ export class CreateVersionDto {
     static create( object: {[key:string]:any}): [string?, CreateVersionDto?] {
         const {title, parentVersion, projectId} = object;
         if (!title) return ['El título de la versión es obligatorio.'];
-        if (!projectId) return ['Debe especificarse el proyecto al que pertenece esta versión.'];
+        if (!projectId) return ['Debe especificar el proyecto al que pertenece esta versión.'];
         return [undefined, new CreateVersionDto(title, parentVersion, projectId)]
     }
 }
