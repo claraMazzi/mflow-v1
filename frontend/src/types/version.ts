7a5bb1f358041b35d2e2dcbc53d886200e2bb485
juan312 <juanalbani48@gmail.com>
2026-02-21 14:01:16 -0300

    Remove server action to improve consistency, Change version entity typing to reduce code repetition

--- frontend/src/types/version.ts
+++ frontend/src/types/version.ts
@@ -5,5 +5,11 @@ export type VersionEntity = {
 	parentVersion: {
 		id: string;
 		title: string;
-	};
+	} | null;
+};
+
+export type SharedVersionEntity = VersionEntity & {
+	parentVersion: { id: string; title: string } | null;
+	projectId: string;
+	projectTitle: string;
 };
